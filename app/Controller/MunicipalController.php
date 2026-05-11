dd58a22b5b1a9aa0f40bf6787027eeb63c19951e
plvhx <foobar.github@gmail.com>
2026-05-11 12:24:57 +0700

    (*): Do major changes.

---
+++ app/Controller/MunicipalController.php
@@ -0,0 +1,247 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pcta\Api\Controller;
+
+use Throwable;
+use OpenApi\Attributes as OpenApi;
+use Pcta\Api\Entity\Municipal;
+use Pcta\Api\Http\Response\Builder as ResponseBuilder;
+use Pcta\Api\Repository\MunicipalRepository;
+use Pcta\Api\Schema\MunicipalSchema;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\RequestInterface as Request;
+use Schnell\Attribute\Route;
+use Schnell\Paginator\Paginator;
+use Schnell\Validator\Validator;
+
+use function class_exists;
+use function sprintf;
+
+// help opcache.preload discover always-needed symbols
+// phpcs:disable
+class_exists(Municipal::class);
+class_exists(ResponseBuilder::class);
+class_exists(MunicipalRepository::class);
+class_exists(MunicipalSchema::class);
+class_exists(Route::class);
+class_exists(Paginator::class);
+class_exists(Validator::class);
+// phpcs:enable
+
+/**
+ * @author Paulus Gandung Prakosa <gandung@infradead.org>
+ */
+class MunicipalController extends BaseController
+{
+    /**
+     * @param \Psr\Http\Message\RequestInterface $request
+     * @param \Psr\Http\Message\ResponseInterface $response
+     * @param array $args
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    #[Route('/municipal', method: 'GET')]
+    #[OpenApi\Get(
+        path: '/municipal',
+        tags: ['Municipal'],
+        responses: [
+            new OpenApi\Response(response: 200, description: 'OK')
+        ]
+    )]
+    public function getAllMunicipals(
+        Request $request,
+        Response $response,
+        array $args
+    ): Response {
+        $repository = new MunicipalRepository(
+            $this->getContainer()->get('mapper'),
+            $request
+        );
+
+        $count = $this->getContainer()
+            ->get('mapper')
+            ->withRequest($request)
+            ->count(new Municipal());
+
+        $paginator = new Paginator($count);
+        $page = $paginator->getMetadata($request);
+
+        return $this->hateoas(
+            $request,
+            $response,
+            $page,
+            $repository->paginate()
+        );
+    }
+
+    /**
+     * @param \Psr\Http\Message\RequestInterface $request
+     * @param \Psr\Http\Message\ResponseInterface $response
+     * @param array $args
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    #[Route('/municipal/{id}', method: 'GET')]
+    #[OpenApi\Get(
+        path: '/municipal/{id}',
+        tags: ['Municipal'],
+        responses: [
+            new OpenApi\Response(response: 200, description: 'OK'),
+            new OpenApi\Response(response: 404, description: 'Not Found')
+        ]
+    )]
+    public function getMunicipalById(
+        Request $request,
+        Response $response,
+        array $args
+    ): Response {
+        $repository = new MunicipalRepository(
+            $this->getContainer()->get('mapper'),
+            $request
+        );
+
+        try {
+            $result = $repository->getById($args['id']);
+        } catch (Throwable $e) {
+            $result = null;
+        }
+
+        if (null === $result) {
+            $builder = new ResponseBuilder();
+            $builder = $builder
+                ->withPair('code', HttpCode::NOT_FOUND)
+                ->withPair('message', sprintf('Municipal with id \'%s\' not found.', $args['id']));
+
+            return $this->json($response, $builder->build(), HttpCode::NOT_FOUND);
+        }
+
+        return $this->json($response, $result);
+    }
+
+    /**
+     * @param \Psr\Http\Message\RequestInterface $request
+     * @param \Psr\Http\Message\ResponseInterface $response
+     * @param array $args
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    #[Route('/province/{pid}/municipal', method: 'POST')]
+    #[OpenApi\Post(
+        path: '/province/{pid}/municipal',
+        tags: ['Municipal'],
+        responses: [
+            new OpenApi\Response(response: 201, description: 'Created'),
+            new OpenApi\Response(response: 404, description: 'Not Found')
+        ]
+    )]
+    public function createMunicipal(
+        Request $request,
+        Response $response,
+        array $args
+    ): Response {
+        $schema = new MunicipalSchema();
+        $validator = new Validator();
+        $validator = $validator->withRequest($request);
+        $validator->assign($schema);
+
+        $repository = new MunicipalRepository(
+            $this->getContainer()->get('mapper'),
+            $request
+        );
+
+        $entity = $repository->create($args['pid'], $schema);
+
+        if (null === $entity) {
+            $builder = new ResponseBuilder();
+            $builder = $builder
+                ->withPair('code', HttpCode::NOT_FOUND)
+                ->withPair('message', sprintf('Province with id \'%s\' not found.', $args['pid']));
+
+            return $this->json($response, $builder->build(), HttpCode::NOT_FOUND);
+        }
+
+        return $this->json($response, $entity, HttpCode::CREATED);
+    }
+
+    /**
+     * @param \Psr\Http\Message\RequestInterface $request
+     * @param \Psr\Http\Message\ResponseInterface $response
+     * @param array $args
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    #[Route('/municipal/{id}', method: 'PUT')]
+    #[OpenApi\Put(
+        path: '/municipal/{id}',
+        tags: ['Municipal'],
+        responses: [
+            new OpenApi\Response(response: 200, description: 'OK'),
+            new OpenApi\Response(response: 404, description: 'Not Found')
+        ]
+    )]
+    public function updateMunicipal(
+        Request $request,
+        Response $response,
+        array $args
+    ): Response {
+        $schema = new MunicipalSchema();
+        $validator = new Validator();
+        $validator = $validator->withRequest($request);
+        $validator->assignOptional($schema);
+
+        $repository = new MunicipalRepository(
+            $this->getContainer()->get('mapper'),
+            $request
+        );
+
+        $entity = $repository->update($args['id'], $schema);
+
+        if (null === $entity) {
+            $builder = new ResponseBuilder();
+            $builder = $builder
+                ->withPair('code', HttpCode::NOT_FOUND)
+                ->withPair('message', sprintf('Municipal with id \'%s\' not found.', $args['id']));
+
+            return $this->json($response, $builder->build(), HttpCode::NOT_FOUND);
+        }
+
+        return $this->json($response, $entity);
+    }
+
+    /**
+     * @param \Psr\Http\Message\RequestInterface $request
+     * @param \Psr\Http\Message\ResponseInterface $response
+     * @param array $args
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    #[Route('/municipal/{id}', method: 'DELETE')]
+    #[OpenApi\Delete(
+        path: '/municipal/{id}',
+        tags: ['Municipal'],
+        responses: [
+            new OpenApi\Response(response: 204, description: 'No Content'),
+            new OpenApi\Response(response: 404, description: 'Not Found')
+        ]
+    )]
+    public function removeMunicipal(
+        Request $request,
+        Response $response,
+        array $args
+    ): Response {
+        $repository = new MunicipalRepository(
+            $this->getContainer()->get('mapper'),
+            $request
+        );
+
+        $entity = $repository->remove($args['id']);
+
+        if (null === $entity) {
+            $builder = new ResponseBuilder();
+            $builder = $builder
+                ->withPair('code', HttpCode::NOT_FOUND)
+                ->withPair('message', sprintf('Municipal with id \'%s\' not found.', $args['id']));
+
+            return $this->json($response, $builder->build(), HttpCode::NOT_FOUND);
+        }
+
+        return $this->response($response, '', HttpCode::NO_CONTENT);
+    }
+}
