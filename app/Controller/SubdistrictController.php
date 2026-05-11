62b64d1ca7a77d21e9b4b4f7e63edb5cc802e7cc
plvhx <foobar.github@gmail.com>
2026-05-11 17:00:29 +0700

    (*): Do major changes.

---
+++ app/Controller/SubdistrictController.php
@@ -0,0 +1,247 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pcta\Api\Controller;
+
+use Throwable;
+use OpenApi\Attributes as OpenApi;
+use Pcta\Api\Entity\Subdistrict;
+use Pcta\Api\Http\Response\Builder as ResponseBuilder;
+use Pcta\Api\Repository\SubdistrictRepository;
+use Pcta\Api\Schema\SubdistrictSchema;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\RequestInterface as Request;
+use Schnell\Attribute\Route;
+use Schnell\Paginator\Paginator;
+use Schnell\Validator\Validator;
+
+use function class_exists;
+use function sprintf;
+
+// help opcache.preload discover always-needed symbols
+// phpcs:disable
+class_exists(Subdistrict::class);
+class_exists(ResponseBuilder::class);
+class_exists(SubdistrictRepository::class);
+class_exists(SubdistrictSchema::class);
+class_exists(Route::class);
+class_exists(Paginator::class);
+class_exists(Validator::class);
+// phpcs:enable
+
+/**
+ * @author Paulus Gandung Prakosa <gandung@infradead.org>
+ */
+class SubdistrictController extends BaseController
+{
+    /**
+     * @param \Psr\Http\Message\RequestInterface $request
+     * @param \Psr\Http\Message\ResponseInterface $response
+     * @param array $args
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    #[Route('/subdistrict', method: 'GET')]
+    #[OpenApi\Get(
+        path: '/subdistrict',
+        tags: ['Subdistrict'],
+        responses: [
+            new OpenApi\Response(response: 200, description: 'OK')
+        ]
+    )]
+    public function getAllSubdistricts(
+        Request $request,
+        Response $response,
+        array $args
+    ): Response {
+        $repository = new SubdistrictRepository(
+            $this->getContainer()->get('mapper'),
+            $request
+        );
+
+        $count = $this->getContainer()
+            ->get('mapper')
+            ->withRequest($request)
+            ->count(new Subdistrict());
+
+        $paginator = new Paginator($count);
+        $page = $paginator->getMetadata($request);
+
+        return $this->hateoas(
+            $request,
+            $response,
+            $page,
+            $repository->paginate()
+        );
+    }
+
+    /**
+     * @param \Psr\Http\Message\RequestInterface $request
+     * @param \Psr\Http\Message\ResponseInterface $response
+     * @param array $args
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    #[Route('/subdistrict/{id}', method: 'GET')]
+    #[OpenApi\Get(
+        path: '/subdistrict/{id}',
+        tags: ['Subdistrict'],
+        responses: [
+            new OpenApi\Response(response: 200, description: 'OK'),
+            new OpenApi\Response(response: 404, description: 'Not Found')
+        ]
+    )]
+    public function getSubdistrictById(
+        Request $request,
+        Response $response,
+        array $args
+    ): Response {
+        $repository = new SubdistrictRepository(
+            $this->getContainer()->get('mapper'),
+            $request
+        );
+
+        try {
+            $result = $repository->getById($args['id']);
+        } catch (Throwable $e) {
+            $result = null;
+        }
+
+        if (null === $result) {
+            $builder = new ResponseBuilder();
+            $builder = $builder
+                ->withPair('code', HttpCode::NOT_FOUND)
+                ->withPair('message', sprintf('Subdistrict with id \'%s\' not found.', $args['id']));
+
+            return $this->json($response, $builder->build(), HttpCode::NOT_FOUND);
+        }
+
+        return $this->json($response, $result);
+    }
+
+    /**
+     * @param \Psr\Http\Message\RequestInterface $request
+     * @param \Psr\Http\Message\ResponseInterface $response
+     * @param array $args
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    #[Route('/district/{did}/subdistrict', method: 'POST')]
+    #[OpenApi\Post(
+        path: '/district/{did}/subdistrict',
+        tags: ['District'],
+        responses: [
+            new OpenApi\Response(response: 201, description: 'Created'),
+            new OpenApi\Response(response: 404, description: 'Not Found')
+        ]
+    )]
+    public function createSubdistrict(
+        Request $request,
+        Response $response,
+        array $args
+    ): Response {
+        $schema = new SubdistrictSchema();
+        $validator = new Validator();
+        $validator = $validator->withRequest($request);
+        $validator->assign($schema);
+
+        $repository = new SubdistrictRepository(
+            $this->getContainer()->get('mapper'),
+            $request
+        );
+
+        $entity = $repository->create($args['did'], $schema);
+
+        if (null === $entity) {
+            $builder = new ResponseBuilder();
+            $builder = $builder
+                ->withPair('code', HttpCode::NOT_FOUND)
+                ->withPair('message', sprintf('District with id \'%s\' not found.', $args['did']));
+
+            return $this->json($response, $builder->build(), HttpCode::NOT_FOUND);
+        }
+
+        return $this->json($response, $entity, HttpCode::CREATED);
+    }
+
+    /**
+     * @param \Psr\Http\Message\RequestInterface $request
+     * @param \Psr\Http\Message\ResponseInterface $response
+     * @param array $args
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    #[Route('/subdistrict/{id}', method: 'PUT')]
+    #[OpenApi\Put(
+        path: '/subdistrict/{id}',
+        tags: ['Subdistrict'],
+        responses: [
+            new OpenApi\Response(response: 200, description: 'OK'),
+            new OpenApi\Response(response: 404, description: 'Not Found')
+        ]
+    )]
+    public function updateSubdistrict(
+        Request $request,
+        Response $response,
+        array $args
+    ): Response {
+        $schema = new SubdistrictSchema();
+        $validator = new Validator();
+        $validator = $validator->withRequest($request);
+        $validator->assignOptional($schema);
+
+        $repository = new SubdistrictRepository(
+            $this->getContainer()->get('mapper'),
+            $request
+        );
+
+        $entity = $repository->update($args['id'], $schema);
+
+        if (null === $entity) {
+            $builder = new ResponseBuilder();
+            $builder = $builder
+                ->withPair('code', HttpCode::NOT_FOUND)
+                ->withPair('message', sprintf('Subdistrict with id \'%s\' not found.', $args['id']));
+
+            return $this->json($response, $builder->build(), HttpCode::NOT_FOUND);
+        }
+
+        return $this->json($response, $entity);
+    }
+
+    /**
+     * @param \Psr\Http\Message\RequestInterface $request
+     * @param \Psr\Http\Message\ResponseInterface $response
+     * @param array $args
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    #[Route('/subdistrict/{id}', method: 'DELETE')]
+    #[OpenApi\Delete(
+        path: '/subdistrict/{id}',
+        tags: ['Subdistrict'],
+        responses: [
+            new OpenApi\Response(response: 204, description: 'No Content'),
+            new OpenApi\Response(response: 404, description: 'Not Found')
+        ]
+    )]
+    public function removeSubdistrict(
+        Request $request,
+        Response $response,
+        array $args
+    ): Response {
+        $repository = new SubdistrictRepository(
+            $this->getContainer()->get('mapper'),
+            $request
+        );
+
+        $entity = $repository->remove($args['id']);
+
+        if (null === $entity) {
+            $builder = new ResponseBuilder();
+            $builder = $builder
+                ->withPair('code', HttpCode::NOT_FOUND)
+                ->withPair('message', sprintf('Subdistrict with id \'%s\' not found.', $args['id']));
+
+            return $this->json($response, $builder->build(), HttpCode::NOT_FOUND);
+        }
+
+        return $this->response($response, '', HttpCode::NO_CONTENT);
+    }
+}
