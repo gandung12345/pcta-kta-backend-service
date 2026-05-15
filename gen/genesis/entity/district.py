8f1392959003dfa8645938c7380d2d13ac598183
plvhx <foobar.github@gmail.com>
2026-05-15 23:24:51 +0700

    (gen/): Adding gen/

---
+++ gen/genesis/entity/district.py
@@ -0,0 +1,57 @@
+# BSD 3-Clause License
+#
+# Copyright (c) 2026, Paulus Gandung Prakosa <gandung@lists.infradead.org>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import uuid
+from datetime import datetime, timezone
+from typing import Optional
+from sqlmodel import Field, Relationship, SQLModel
+
+def get_utc_now():
+    return datetime.now(timezone.utc)
+
+class District(SQLModel, table=True):
+    __tablename__ = 'district'
+
+    id: uuid.UUID = Field(
+        default_factory=uuid.uuid4,
+        primary_key=True,
+        index=True,
+        nullable=False
+    )
+    formalIdentifier: str = Field(nullable=False)
+    formalName: str = Field(nullable=False)
+    createdAt: datetime = Field(default_factory=get_utc_now, nullable=False)
+    updatedAt: datetime = Field(
+        default_factory=get_utc_now,
+        sa_column_kwargs={'onupdate': get_utc_now},
+        nullable=False
+    )
+    municipalRefId: Optional[uuid.UUID] = Field(default=None, foreign_key='municipal.id')
+    municipal: Optional[Municipal] = Relationship()
---
+++ gen/genesis/task/province.py
@@ -0,0 +1,49 @@
+# BSD 3-Clause License
+#
+# Copyright (c) 2026, Paulus Gandung Prakosa <gandung@lists.infradead.org>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import uuid
+
+from genesis.entity.province import Province
+from genesis.task.base import AbstractTask
+from sqlmodel import Session
+from tqdm import trange
+
+class CreateBulkProvinceTask(AbstractTask):
+    def invoke(self, engine):
+        session = Session(engine)
+
+        for i in trange(len(self.chunks)):
+            session.add(Province(formalIdentifier=self.chunks[i][0], formalName=self.chunks[i][1].upper()))
+
+        session.commit()
+        session.close()
+
+    def guid(self):
+        return '<create-bulk-province-task:%s>' % str(uuid.uuid4())
