5f16f52deca479166f27b8dd6678738f6422e523
plvhx <foobar.github@gmail.com>
2026-05-23 18:49:51 +0700

    (*): Do minor changes.

--- app/Entity/Municipal.php
+++ app/Entity/Municipal.php
@@ -99,8 +99,11 @@ class Municipal extends AbstractEntity
     #[Json(name: 'createdAt')]
     #[OpenApi\Property(
         property: 'createdAt',
-        type: 'timestamp',
+        type: 'string',
+        format: 'datetime',
         description: 'Municipal created at',
+        example: '1970-01-01 00:00:00',
+        pattern: '^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}',
         readOnly: true
     )]
     private ?DateTime $createdAt;
@@ -112,8 +115,11 @@ class Municipal extends AbstractEntity
     #[Json(name: 'updatedAt')]
     #[OpenApi\Property(
         property: 'updatedAt',
-        type: 'timestamp',
+        type: 'string',
+        format: 'datetime',
         description: 'Municipal updated at',
+        example: '1970-01-01 00:00:00',
+        pattern: '^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}',
         readOnly: true
     )]
     private ?DateTime $updatedAt;
